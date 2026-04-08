cc0843d38dd11da558f81624c99f14e4d6b317fc
mdyasar49 <mohamedyasar081786@gmail.com>
2026-04-08 21:15:31 +0530

    feat: add API_BASE_URL configuration using environment variable

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://mern-portfolio-yasar.onrender.com/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export default API_BASE_URL;
