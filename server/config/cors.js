ba3e04da0dc23b49c8a745cab441d6bc8994b780
mdyasar49 <mohamedyasar081786@gmail.com>
2026-04-16 08:22:10 +0530

    feat: initialize server and client environment configurations, CORS middleware, and portfolio data structures

--- server/config/cors.js
+++ server/config/cors.js
@@ -12,6 +12,8 @@ const getAllowedOrigins = () => {
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
+    'http://localhost:2003',
+    'http://127.0.0.1:2003',
     'https://mern-portfolio-yasar-1.onrender.com'
   ];
 
